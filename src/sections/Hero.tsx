7c63763e6d97144c559a52d52cce3a945f371ca6
Bilha <bilhamwengi34@gmail.com>
2025-01-02 10:17:48 +0300

    hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,78 @@
+import memoji from "@/assets/images/memoji.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import HeroOrbit from "@/components/HeroOrbit";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32 md:py-48 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{ backgroundImage: `url(${grainImage.src})` }}
+        ></div>
+        <div className="size-[600px] hero-ring"></div>
+        <div className="size-[800px] hero-ring"></div>
+        <div className="size-[1000px] hero-ring"></div>
+        <div className="size-[1200px] hero-ring"></div>
+        <HeroOrbit size={800} rotation={-74}>
+          <StarIcon className="size-24 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={450} rotation={-70}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={120}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-28}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={400} rotation={240}>
+          <StarIcon className="size-5 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={110}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={-90}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={700} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+      </div>
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image src={memoji} alt="Memoji" className="size-[100px]" />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-2xl md:text-4xl text-center mt-8 tracking-wide">
+            Crafting Outstanding User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-base">
+            Focused on delivering intuitive, high-performing web applications
+            tailored to your needs. Let&apos;s make your vision a reality.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
+            <span>👋🏽</span>
+            <span className="font-semibold">Let&apos;s connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
